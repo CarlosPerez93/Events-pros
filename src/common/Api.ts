6d7466c5231a8ea35e2571ee1ed487ca186b3b60
Carlosperez2 <Carlosperez20131015@gmail.com>
2023-08-02 19:19:49 +0200

    feat(app): use globals url  backend

--- src/common/Api.ts
+++ src/common/Api.ts
@@ -1,18 +1,19 @@
+import "dotenv";
 import axios from "axios";
 import { Api } from "./api.tpye";
 
-const url = "http://localhost:4000/api";
+const URL = import.meta.env.VITE_BACKEND_URL;
 
 const Api = async (props: Api) => {
   const { data, params, patch, token } = props;
   if (patch === "post") {
-    return await axios.post(`${url}${params}`, data, { headers: token });
+    return await axios.post(`${URL}${params}`, data, { headers: token });
   } else if (patch === "get") {
-    return await axios.get(`${url}${params}`);
+    return await axios.get(`${URL}${params}`);
   } else if (patch === "put") {
-    return await axios.put(`${url}${params}`, data);
+    return await axios.put(`${URL}${params}`, data);
   } else if (patch === "delete") {
-    return await axios.delete(`${url}${params}`);
+    return await axios.delete(`${URL}${params}`);
   }
 };
 
